0a45f07b2d5780b11aaadb2446d89ac0a685cb46
vishalamc <vishalamc@gmail.com>
2021-08-27 16:53:26 +0530

    Add files via upload

---
+++ formInput.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+	<title>Login Page</title>
+</head>
+<body background="backgroundimg4.webp">
+
+<form method="post" action="success.php"  autocomplete="off">
+<table height="50%" align="center" width="20%" border="2">
+<tr><td><img src="login3.jpg" height="115px" width="100%"></td></tr>
+<tr><td>&nbsp;</td></tr>
+<tr><td>
+  <input type="text" name="username" placeholder="Enter userName" required />
+</td>
+</tr>
+<tr><td>
+  <input type="email" name="email" placeholder="Enter email Id" required />
+</td>
+</tr>
+<tr><td>
+  <input type="password" name="mypwd" placeholder="Enter password" required />
+</td>
+</tr>
+<tr><td>
+  <input type="submit" name="btnsubmit" value="SUBMIT" />
+</td></tr>
+</table>
+</form>
+
+
+</body>
+
+</html>
